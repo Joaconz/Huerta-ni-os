3e368cf1f7e359f9c39600dde9db2473c9ccd3f7
nehuenigds <nehuenigds@outlook.com>
2022-05-24 03:12:52 -0300

    elegir canales

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,20 @@ struct riego{
   int intervalo=2;         
   int tiempo=20;   
 };
-
 int n=1;
+int IByte;
+struct riego lista[5];
 
-struct riego lista;
 
 void valvulasEstado(int i, bool b){
   digitalWrite(i+n, b);
-  Serial.print("estado = ");
+
+  Serial.print("valvula: ");
+  Serial.print(i);
+  Serial.print("  estado: ");
   Serial.println(b);
 }
+
 
 int minute(){
   time_t now;
@@ -23,36 +27,72 @@ int minute(){
   return (localtime(&now)->tm_min);
 }
 
+
 int second(){
   time_t now;
   time(&now);
   return (localtime(&now)->tm_sec);
 }
+
+
 bool sameTime(int i){
-  if((!(minute()%lista.intervalo)) && (second()<=lista.tiempo)) return 1;
+  if((!(minute()%lista[i].intervalo)) && (second()<=lista[i].tiempo)) return 1;
   else return 0;
 }
+
+
 void printDigits(int digits){
-  // utility function for digital clock display: prints preceding colon and leading 0
   Serial.print(":");
   if(digits < 10)
     Serial.print('0');
   Serial.print(digits);
 }
 
+
+void changeBool(bool &a){
+  if (a) a=0;
+  else a=1;  
+}
+
+
+void modificarCanal(){
+  while (!(Serial.available()>0)){
+    delay(10);}
+  int incomingByte= Serial.read()-'1' ;
+  changeBool(lista[incomingByte].estado);
+
+  Serial.print("estado cambiado. Valvula: ");
+  Serial.print(incomingByte);
+  Serial.print("  Estado: ");
+  Serial.println(lista[incomingByte].estado);
+}
+
+
 void setup() {
   Serial.begin(9600);
   //setPines();
 }
 
+
 void loop() {
-  for (int i = 0; i < 1; i++){
-    if(sameTime(i) && lista.estado) valvulasEstado(i,1);
+
+  if (Serial.available() > 0) {
+    // leer sigiente byte
+    IByte = Serial.read();
+    switch (IByte){
+    case 'A'://modificar estado de canal n
+      delay(100);
+      modificarCanal();
+      break;
+    default:
+      break;
+    }
+  }
+
+  for (int i = 0; i < 5; i++){
+    if(sameTime(i) && lista[i].estado) valvulasEstado(i,1);
     else valvulasEstado(i,0);
   }
-  Serial.print(minute());
-  printDigits(second());
-  Serial.println();
-  delay(1000);
+  delay(50);
 }
 
